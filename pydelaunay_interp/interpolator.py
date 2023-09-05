0f5d379d8485dea7118010dffef9891f91208491
Alexey Belkov <fpmbelkov@gmail.com>
2023-09-05 19:50:03 +0300

    new benchmarks

--- pydelaunay_interp/interpolator.py
+++ pydelaunay_interp/interpolator.py
@@ -5,19 +5,19 @@ from typing import Union
 
 class Linear2DInterpolator(BiLinearInterpolator):
     def __init__(self, points: np.ndarray[Union[np.int32, np.double]], values: np.ndarray[np.float32], **kwargs):
-        print('starting triangulation ....')
+        # print('starting triangulation ....')
         super().__init__(points, values)
-        print('fininshed triangulation')
-        print('starting tree building')
+        # print('fininshed triangulation')
+        # print('starting tree building')
         self.kdtree = KDTree(data=self.triangulation.vertices, **kwargs)
-        print('finished tree building')
+        # print('finished tree building')
 
     def __call__(self, points: np.ndarray[Union[np.int32, np.double]], fill_value: float = 0.0, workers: int = -1) -> np.ndarray[np.float32]:
-        print('starting query')
+        # print('starting query')
         _, neighbors = self.kdtree.query(points, 1, workers=workers)
         # neighbors = np.ones((100,))
-        print('finished query')
-        print('starting interpolation')
+        # print('finished query')
+        # print('starting interpolation')
         int_values: np.ndarray[np.float32] = super().__call__(points, neighbors, fill_value)
-        print('finished interpolation')
+        # print('finished interpolation')
         return int_values
